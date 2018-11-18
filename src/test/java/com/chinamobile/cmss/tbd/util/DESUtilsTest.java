a1ed7c1a165060b2fa521c4c7b79a2595ff16fad
koudandan <15769156764>
2018-11-18 19:20:27 +0800

    王婆卖瓜

---
+++ src/test/java/com/chinamobile/cmss/tbd/util/DESUtilsTest.java
@@ -0,0 +1,24 @@
+package com.chinamobile.cmss.tbd.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: koudandan
+ * @Date: 2018/11/18 19:18
+ */
+public class DESUtilsTest {
+
+    @Test
+    public void getEncryptString() {
+    }
+
+    @Test
+    public void getDecryptString() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
